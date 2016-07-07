065d11d9b661309e8b6283d6eba8f4a538e59b98
xiechao06 <xiechao06@gmail.com>
2016-07-07 14:12:38 +0800

    0

--- ch2/2.41.scm
+++ ch2/2.41.scm
@@ -5,15 +5,16 @@
             (cons next (iter (+ next 1)))))
         (iter n))
 
+
 (define (unique-pairs n)
-        (flatmap (lambda (x) 
+        (append-map (lambda (x) 
                    (map (lambda (y) (list y x)) (enumerate-interval 1 x))) 
                  (enumerate-interval 1 n)))
 
 
 (define (unique-tripples n)
         (append-map (lambda (x) 
-                      (map (lambda (pair) (append pair x)) (unique-pairs x))) 
+                      (map (lambda (pair) (append pair (list x))) (unique-pairs x))) 
                     (enumerate-interval 3 n)))
 
 ; tests
