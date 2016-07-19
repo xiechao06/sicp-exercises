2ba43799566ae5a7e48217bd11d70687557de76b
xiechao06 <xiechao06@gmail.com>
2016-07-19 09:16:04 +0800

    0

--- ch2/2.61.scm
+++ ch2/2.61.scm
@@ -1,6 +1,10 @@
 (define (adjoin-set x set)
-        (define (iter traveled rest) 
-          (if (null? rest) ())))
+        (let ((head (car set))) 
+          (cond	((null? set) (cons x ()))
+                ((= head x) (cons x (cdr set)))
+                ((< head x) (cons head (adjoin-set x (cdr set))))
+                (else (cons x set))))
+        )
 
 ; tests
-(adjoin-set 3 (list 1 2 4))
+(adjoin-set 3 (list 1 2 4 5))
